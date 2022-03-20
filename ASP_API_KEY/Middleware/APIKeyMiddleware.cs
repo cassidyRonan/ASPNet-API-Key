d5500b1d5e409467192dd2406534b9c482af5d85
Ronan Cassidy <ronanmcassidy@gmail.com>
2022-03-20 14:14:54 +0000

    Add APIKey Middleware
    - Adds API Key Middleware class to project

---
+++ ASP_API_KEY/Middleware/APIKeyMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace ASP_API_KEY.Middleware
+{
+    /// <summary>
+    /// This middleware will check the API key in the header and validate the key by extracting it from the header and compare the key defined in code.
+    /// </summary>
+    public class APIKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string API_KEY = "XApiKey";
+
+        public APIKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!context.Request.Headers.TryGetValue(API_KEY, out var extractedKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key was not provided");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(API_KEY);
+            if (!apiKey.Equals(extractedKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
